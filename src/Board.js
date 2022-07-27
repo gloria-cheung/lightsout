12746873eae7c5e3215ca888c362822b9f43db34
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-07-26 20:36:52 -0400

    initial commit

---
+++ src/Board.js
@@ -0,0 +1,73 @@
+import {useState} from "react";
+import Cell from "./Cell";
+import './Board.css';
+
+/** Game board of Lights out.
+ * Properties:
+ * - nrows: number of rows of board
+ * - ncols: number of cols of board
+ * - chanceLightStartsOn: float, chance any cell is lit at start of game
+ *
+ * State:
+ * - hasWon: boolean, true when board is all off
+ * - board: array-of-arrays of true/false
+ *
+ *    For this board:
+ *       .  .  .
+ *       O  O  .     (where . is off, and O is on)
+ *       .  .  .
+ *
+ *    This would be: [[f, f, f], [t, t, f], [f, f, f]]
+ *
+ *  This should render an HTML table of individual <Cell /> components.
+ *  This doesn't handle any clicks --- clicks are on individual cells
+ **/
+function Board(props) {
+  const [state, setState] = useState({
+
+  });
+
+  const createBoard = function() {
+    let board = [];
+    // TODO: create array-of-arrays of true/false values
+    return board
+  };
+
+   /** handle changing a cell: update board & determine if winner */
+
+   const flipCellsAround = function(coord) {
+    let {ncols, nrows} = props;
+    let board = state.board;
+    let [y, x] = coord.split("-").map(Number);
+
+    function flipCell(y, x) {
+      // if this coord is actually on board, flip it
+
+      if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
+        board[y][x] = !board[y][x];
+      }
+   };
+     // TODO: flip this cell and the cells around it
+
+    // win when every cell is turned off
+    // TODO: determine is the game has been won
+
+    // setState({board, hasWon});
+  }
+
+  // if the game is won, just show a winning msg & render nothing else TODO
+  // make table board TODO
+  return (
+    <table className="Board">
+      <tbody>
+        <tr>
+          <Cell isLit={false}/>
+          <Cell isLit={true}/>
+          <Cell isLit={false}/>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+export default Board;
